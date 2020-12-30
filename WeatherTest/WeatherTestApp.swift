4e81c9793e55b6fe84b7e8a4091814c6a2b33be3
Christian Tanputra <christian_tanputra@icloud.com>
2020-12-30 19:51:21 +0700

    Initial Commit

---
+++ WeatherTest/WeatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestApp.swift
+//  WeatherTest
+//
+//  Created by Christian Tanputra on 30/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
